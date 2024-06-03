87f33474d4362295111569462dddfccaa6064e8c
Victor Kauan <victorkauan.dev@gmail.com>
2024-06-03 01:13:22 -0300

    :sparkles: feat: Adjust home screen style

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,116 +1,71 @@
-import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import { theme } from "@/theme";
-import { Href, Link } from "expo-router";
+import Card from '@/components/home/Card'
+import { theme } from '@/theme'
+import { Feather } from '@expo/vector-icons'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
-export default function home() {
+export default function HomePage() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.logoutContainer}>
-        <Text style={{ color: "#f5f5f5" }}>Sair da conta</Text>
+        <Text style={{ color: '#f5f5f5' }}>Sair da conta</Text>
         <Feather name="log-out" size={20} color="#BC5252" />
-        {/* colors.others.red */}
       </View>
-      <View style={styles.mainContainer}>
-        <Image source={require("../../assets/logo.png")} />
 
-        <View style={{ width: "100%", alignItems: "center", gap: 24 }}>
-          <Text style={styles.title}>
-            Bem-vindo,{" "}
-            <Text
-              style={{
-                textDecorationLine: "underline",
-                lineHeight: 44,
-                fontFamily: theme.fonts.family.bold,
-              }}
-            >
-              Username!
-            </Text>
-          </Text>
+      <Image
+        source={require('../../assets/logo.png')}
+        style={styles.logo}
+        alt="Phais+ logo"
+      />
 
-          <View style={styles.cards}>
-            <Card title="Solicitações" icon="inbox" path="/requests" />
-            <Card title="Medicamentos" icon="activity" path="/meds" />
-            <Card title="Minha Conta" icon="user" path="/profile" />
-            <Card title="Sair da Conta" icon="log-out" path="/" />
-          </View>
-        </View>
+      <Text style={styles.title}>
+        Bem-vindo, <Text style={styles.titleUsername}>Username!</Text>
+      </Text>
+
+      <View style={styles.cards}>
+        <Card title="Solicitações" icon="inbox" path="/requests" />
+        <Card title="Medicamentos" icon="activity" path="/meds" />
+        <Card title="Minha Conta" icon="user" path="/profile" />
+        <Card title="Sair da Conta" icon="log-out" path="/" />
       </View>
     </SafeAreaView>
-  );
-}
-
-interface CardProps {
-  title: string;
-  icon: any;
-  path: Href<string>;
-}
-function Card({ title, icon, path }: CardProps) {
-  return (
-    <Link href={path} style={{ width: "40%" }}>
-      <View style={[styles.card, styles.shadowProp]}>
-        <Feather name={icon} size={44} color={theme.colors.neutral.sec} />
-        <Text style={styles.cardTitle}>{title}</Text>
-      </View>
-    </Link>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    backgroundColor: "#2C3A3B",
-    gap: 24,
-    paddingTop: 35,
-  },
-  mainContainer: {
-    flex: 1,
-    width: "100%",
-    alignItems: "center",
-    marginTop: 34,
-    backgroundColor: theme.colors.bg.main,
-    gap: 44,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2C3A3B',
+    paddingHorizontal: 24,
   },
   logoutContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
     gap: 8,
-    width: "90%",
+    position: 'absolute',
+    top: 64,
+    right: 24,
+  },
+  logo: {
+    marginBottom: 36,
   },
   title: {
-    color: theme.colors.neutral.sec,
-    fontSize: 22,
+    color: theme.colors.neutral['200'],
+    fontSize: 24,
     fontFamily: theme.fonts.family.medium,
+    marginBottom: 20,
+  },
+  titleUsername: {
+    textDecorationLine: 'underline',
   },
   cards: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    gap: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: 16,
   },
-  card: {
-    width: "100%",
-    height: 150,
-    backgroundColor: theme.colors.bg.layer,
-    borderRadius: 15,
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-  },
-  cardTitle: {
-    color: theme.colors.neutral.sec,
-    fontSize: 20,
-    fontFamily: theme.fonts.family.regular,
-  },
-  shadowProp: {
-    shadowColor: "#171717",
-    shadowOffset: { width: 2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-});
+})
