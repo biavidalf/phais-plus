0b39b811625c1cd242f31577a6328aa9f7a754c9
Victor Kauan <victorkauan.dev@gmail.com>
2024-06-06 23:19:58 -0300

    :sparkles: feat: Create show medicine service

--- mobile/src/api/medicine.ts
+++ mobile/src/api/medicine.ts
@@ -4,6 +4,9 @@ const medicineService = {
   async index() {
     return await api.get('/medicine')
   },
+  async show(id: string) {
+    return await api.get(`/medicine/${id}`)
+  },
 }
 
 export default medicineService
